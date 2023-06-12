f5ef908c7c73456e958fdc6ee0c6b03febea80ca
sabin-icanio <134059064+sabin-icanio@users.noreply.github.com>
2023-06-12 13:18:27 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p style="font-size:80">
+        <p>
           DOCKER COMPOSE
         </p>
         <a
